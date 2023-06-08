1392554ead0b66b53e04a509fe6295b767df9d2c
MLibralato91 <michelelibralato911@gmail.com>
2023-06-08 18:06:16 +0200

    add types

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
+    protected $fillable = ['title', 'image', 'description'];
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
